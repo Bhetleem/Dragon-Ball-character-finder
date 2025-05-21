0d6c500f2ce5a512fabb005e75dcba958bc21b18
Belén <bhetleem@gmail.com>
2025-05-21 13:03:37 +0200

    Vite configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Bhetleem/Dragon-Ball-character-finder/",
+  base: "/Dragon-Ball-character-finder/",
 })
